1f87c270c93f0c1e890af9a6c4284fee9236ae50
Gahee Kim <rkgml981105@gmail.com>
2020-11-14 15:29:43 +0900

    modify schema, views

--- schemas/user.js
+++ schemas/user.js
@@ -42,7 +42,11 @@ const userSchema = new Schema({
       type: Date,
       default: Date.now,
       required: true
-  }
+  },
+  role: {
+    type: Number,
+    default: 0
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
