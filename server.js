26cf9f7c551d6cdcd7f46b4cb54e2df1fa6f0f54
BaeJiEun <bje1703@naver.com>
2020-12-23 02:44:30 +0900

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 const session = require("express-session");
 const ejs = require("ejs");
 const connect = require("./schemas");
+const bodyParser = require('body-parser'); // 1
+const methodOverride = require('method-override'); // delete, update 
 
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
+app.use(methodOverride('_method')); // delete, update 
 
 connect();
 
@@ -34,18 +39,13 @@ app.set("views",__dirname + "/views");
 app.set("view engine", "ejs");
 app.engine('ejs', ejs.renderFile);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.use("/member", require("./router/memberRouter"));
 app.use("/board", require("./router/boardRouter"));
 
 app.use("/", require('./router/controller'));
 
-
 app.listen(3000, () => {
   console.log("서버 가동합니당");
 });
-
-
-
-
