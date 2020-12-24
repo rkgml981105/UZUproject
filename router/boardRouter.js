6cf0ac61ffbc221210f42bf556a33c2c81729b3a
wonkyeong <lwk9819@naver.com>
2020-12-24 16:43:15 +0900

    short crud css

--- router/boardRouter.js
+++ router/boardRouter.js
@@ -6,17 +6,6 @@ const Board_long = require("../schemas/board_long");
 //지은 사이트  https://supdev.tistory.com/37  - boardlist, show, new(&mongo에 insert)
 
 /* boardlist */
-<<<<<<< HEAD
-router.get('/long', function (req, res,next) {
-  Board.find({}, function (err, board) {
-      res.render('board/long/boardlist.ejs', { title: 'Board', board: board });
-  })
-})
-
-router.get('/short', function (req, res,next) {
-  Board.find({}, function (err, board) {
-      res.render('board/short/boardlist.ejs', { title: 'Board', board: board });
-=======
 
 router.get('/long', function (req, res) {
   Board_long.find({})
@@ -33,7 +22,6 @@ router.get('/short', function (req, res) {
   .exec(function (err, boards) {
     if(err) return res.json(err);
     res.render('board/short/boardlist.ejs', { boards: boards });
->>>>>>> 86ef3dc3248cc1a10dca88d083c85c82462e7273
   })
 })
 
@@ -64,29 +52,6 @@ router.post('/long/write', function(req, res){
 });
 
 
-<<<<<<< HEAD
-/*show board*/
-router.get('/best/boardlist', function(req, res) {
-  res.render('board/best/boardlist', { title: '글쓰기' });
-});
-
-router.get('/long/boardlist', function(req, res) {
-  res.render('board/long/boardlist.ejs', { title: '글쓰기' });
-});
-
-router.get('/short/boardlist', function(req, res) {
-  res.render('board/short/boardlist.ejs', { title: '글쓰기' });
-});
-
-
-/* write(new)  */
-router.get('/long/write', function(req, res) {
-  res.render('/board/long/write.ejs', { title: '글쓰기' });
-});
- 
-router.get('/short/write', function(req, res, next) {
-  res.render('/board/short/write.ejs', { title: '글쓰기' });
-=======
 router.post('/short/write', function(req, res){
   Board.create(req.body, function(err, board){
     if(err) return res.json(err);
@@ -102,7 +67,6 @@ router.get('/long/:id', function (req, res) {
       if(err) return res.json(err);
       res.render('board/long/show', { board_long: board_long });
   })
->>>>>>> 86ef3dc3248cc1a10dca88d083c85c82462e7273
 });
 
 
@@ -120,10 +84,6 @@ router.get('/best/:id', function (req, res) {
       res.render('board/best/show', {boards: boards });
   })
 });
-
-
-//원경 사이트 https://www.a-mean-blog.com/ko/blog/Node-JS-%EC%B2%AB%EA%B1%B8%EC%9D%8C/%EC%A3%BC%EC%86%8C%EB%A1%9D-%EB%A7%8C%EB%93%A4%EA%B8%B0/%EC%A3%BC%EC%86%8C%EB%A1%9D-Show-Edit-Update-Destroy
-//edit(&mongo에 update),  destroy(삭제)
 
 /* edit */
 router.get('/long/:id/edit', function(req, res){
