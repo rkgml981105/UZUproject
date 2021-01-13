3fbbc9c0e81634107d0b4c22aa27ec05d4f3a25b
Gahee Kim <rkgml981105@gmail.com>
2021-01-13 16:20:36 +0900

    일단 userSchema 맨 밑에 있는 password validation 주석처리 (비밀번호 유효성검사 통과 못해도 회원가입 가능하게)

--- schemas/user.js
+++ schemas/user.js
@@ -56,16 +56,16 @@ const userSchema = new Schema({
     }
   });
 
-  //password validation 
-  var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // 2-1
-  var passwordRegexErrorMessage = '8자 이상이어야 하고, 숫자와 문자를 혼용해야 합니다'; // 2-2
-  userSchema.path('password').validate(function(v) {
-    var user = this;
+  // //password validation 
+  // var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // 2-1
+  // var passwordRegexErrorMessage = '8자 이상이어야 하고, 숫자와 문자를 혼용해야 합니다'; // 2-2
+  // userSchema.path('password').validate(function(v) {
+  //   var user = this;
 
-    if(!passwordRegex.test(user.password)){ // 2-3
-       user.invalidate('password', passwordRegexErrorMessage); // 2-4
-    } 
-  });
+  //   if(!passwordRegex.test(user.password)){ // 2-3
+  //      user.invalidate('password', passwordRegexErrorMessage); // 2-4
+  //   } 
+  // });
 
 module.exports = mongoose.model("User", userSchema);
 
